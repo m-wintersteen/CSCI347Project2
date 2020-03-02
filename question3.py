c3a1270b56b34be907b45eafefefb9274477fcbd
BruceClark406 <brucewestonmt@gmail.com>
2020-03-02 13:20:26 -0700

    Update question3.py

--- question3.py
+++ question3.py
@@ -4,22 +4,29 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-#visualize our Graph
+
+# visualize our Graph
 def visualize_Graph():
-    edge_list = preprocessing.slice_dataset()
-    G = nx.Graph()
-    G.add_edges_from(edge_list)
-    nx.draw_networkx(G, with_labels=True, node_size=1)
+    nx.draw_networkx(G, with_labels=False, node_size=1)
     plt.show()
+
 
 # top 10 by betweenness
 def top_betweenness():
-    Project2.getBC()
-
-
+    betweenness = []
+    for i in node_list
+        print(i)
+        betweenness.append(Project2.getBC(edge_list, i))
+    print(type(betweenness))
 
 
 if __name__ == "__main__":
-    visualize_Graph()
-    #print(preprocessing.slice_dataset())
+    G = nx.Graph()
+    edge_list = preprocessing.slice_dataset()
+    G.add_edges_from(edge_list)
+    node_list = G.nodes()
 
+
+    #visualize_Graph()
+
+    top_betweenness()
