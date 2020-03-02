b47409f957be76c2820bda7647abc74d080538ea
NorhanAbbas <48738927+NorhanAbbas@users.noreply.github.com>
2020-03-02 16:19:31 -0700

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,22 @@
 import csv
 from itertools import islice
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def slice_dataset():
     with open('dataset/amazon0302.csv') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         nodes = []
         edge_list = []
+        G = nx.Graph()
 
 
     # the total number of pairs = 1048575
     # we are using only 8000
 
         for row in islice(csvreader, 1047575, None):
+        #for row in csvreader:
 
             fromNode = int(row[0])
             toNode = int(row[1])
@@ -24,9 +28,13 @@ def slice_dataset():
             edge_list.append(edge)
 
 
-        return(edge_list)
+        #return(edge_list)
+        G.add_edges_from(edge_list)
+        H = G.subgraph(nodes)
+        nx.draw_networkx(H, with_labels=False)
+        plt.show()
 
 
-if __name__ == "__main__":
-    edge_list = slice_dataset()
-    print(edge_list)
+#print(slice_dataset())
+slice_dataset()
+
