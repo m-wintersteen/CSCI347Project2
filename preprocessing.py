63edeb4546ddf1853fa0eec0a3c802a7cf38d9cf
m-wintersteen <muniwintersteen@gmail.com>
2020-03-02 17:46:41 -0700

    Added random subgraph algo

--- preprocessing.py
+++ preprocessing.py
@@ -3,10 +3,10 @@ from itertools import islice
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 def connected_component_subgraphs(G):
     for c in nx.connected_components(G):
-        print("subgraph")
         yield G.subgraph(c)
 
 def slice_dataset():
@@ -38,24 +38,15 @@ def slice_dataset():
         H = G.subgraph(nodes)
         nx.draw_networkx(H, with_labels=False)
         plt.show()
-def reduce():
+def randomreduce():
     with open('dataset/amazon0302.csv') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
-        nodes = []
         edge_list = []
         G = nx.Graph()
-
-
-    # the total number of pairs = 1048575
-    # we are using only 8000
-
+        
         for row in csvreader:
-
             fromNode = int(row[0])
             toNode = int(row[1])
-
-            nodes.append(fromNode)
-            nodes.append(toNode)
 
             edge = [fromNode, toNode]
             edge_list.append(edge)
@@ -63,21 +54,22 @@ def reduce():
 
         #return(edge_list)
         G.add_edges_from(edge_list)
-        count = 0
-        for subgraph in connected_component_subgraphs(G):
-            file_name = "reduced_data/reduce{}.edgelist".format(count)
-            nx.write_edgelist(subgraph, file_name)
-            count += 1
-        #nx.draw_networkx(Gc, with_labels=False)
-        #plt.show()
-    
+        for n in list(G.nodes):
+            if random.randint(0,100) < 84:
+                G.remove_node(n)
+        return G
 
 
 #print(slice_dataset())
 #slice_dataset()
-reduce()
+G = randomreduce()
 
-#G=nx.read_edgelist("reduced_data/reduce0.edgelist")
-#nx.draw_networkx(G, with_labels=False)
+Gc = max(connected_component_subgraphs(G), key=len)
+file_name = "reduced_data/reduce_random.edgelist"
+nx.write_edgelist(Gc, file_name)
+
+#nx.draw_networkx(Gc, with_labels=False)
 #plt.show()
+
+
 
